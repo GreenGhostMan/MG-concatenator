7f014516d6a5ce520672e89e1328753306df765b
rom robotics <server01.psa1981@gmail.com>
2021-09-06 16:55:33 +0630

    add oxy sensor

--- MG/MG.ino
+++ MG/MG.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <SoftwareSerial.h>
 
 int compressor=12;
 int fan=11;
@@ -6,7 +7,7 @@ int solA=10;
 int solB=9;
 
 int pressure_sensor = A0; int pressure_high = 90; int pressure_low = 20;
-int purity_sensor = A1;
+int purity_sensor = A1; int purity_high = 96; int purity_low = 50; // oxygen analyzer
 
 int delay_millis = 1000;
 
@@ -16,10 +17,19 @@ void powerOff();
 void check_purity();
 void check_pressure();
 void check_all();
+void testPrint();
 
+SoftwareSerial mySerial(3,2); // Rx, Tx
+SoftwareSerial mySerial2(4,5); // Rx, Tx
 void setup()
 
 { 
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  //mySerial2.begin(9600);
+  //while(!Serial){};
+  //while(!mySerial){};
+  
   pinMode (compressor,OUTPUT);
   pinMode (fan,OUTPUT);
   pinMode (solA,OUTPUT);
@@ -36,10 +46,12 @@ void setup()
 
 void loop()
 {  
-  Right();
-  delay(delay_millis);
-  Left();
-  delay(delay_millis);
+//  Right();
+//  delay(delay_millis);
+//  Left();
+//  delay(delay_millis);
+
+  testPrint();
   check_all();
 }
 
@@ -66,16 +78,33 @@ void powerOff()
 
 void check_purity() 
 {
-  
-}
-void check_pressure()
-{
-  if ( pressure > pressure_high )
+  purity_sensor = 1*purity_sensor;  // volt to real sensor value
+   
+    if ( purity_sensor > purity_high )
   {
     // alarm
     // power off
   }
-  else if (pressure < pressure_low)
+  else if (purity_sensor < purity_low)
+  {
+    // only alarm
+  }
+  else 
+  {
+    // all ok
+  } 
+}
+
+
+void check_pressure()
+{
+  pressure_sensor = 1*pressure_sensor; // volt to real sensor
+  if ( pressure_sensor > pressure_high )
+  {
+    // alarm
+    // power off
+  }
+  else if (pressure_sensor < pressure_low)
   {
     // only alarm
   }
@@ -92,3 +121,17 @@ void check_all()
   check_purity();
   check_pressure();
 }
+
+void testPrint()
+{
+//  if( mySerial.available() )
+//  {
+//    Serial.write( mySerial.read() );
+//  }
+
+    while (mySerial.available()> 12) {
+    char inChar = (char)mySerial.read();
+    Serial.write(inChar);
+    //mySerial2.print(inChar);
+  }
+ }
